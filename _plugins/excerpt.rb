4b0ab31e9b1c0c38cd44fc8b86de426d99b2a731
WouterJ <waldio.webdesign@gmail.com>
2013-02-10 13:32:06 +0100

    Improved some CS

--- _plugins/excerpt.rb
+++ _plugins/excerpt.rb
@@ -1,7 +1,7 @@
 module Jekyll
-  module Filters
-    def excerpt(content)
-      content.match('<!--more-->') ? content.split('<!--more-->').first : content
+    module Filters
+        def excerpt(content)
+            content.match('<!--more-->') ? content.split('<!--more-->').first : content
+        end
     end
-  end
 end
