38fe33c028515ecc7c287bbbaa2a6eeb0c8c3477
WouterJ <waldio.webdesign@gmail.com>
2013-02-11 17:56:19 +0100

    Added support for thumbnail on post pages

--- _plugins/thumbnail.rb
+++ _plugins/thumbnail.rb
@@ -16,4 +16,20 @@ module Jekyll
 
     end
 
+    class Page
+    
+        alias_method :to_liquid_org, :to_liquid
+        def to_liquid
+            vars = to_liquid_org
+
+            if vars.has_key?('thumbnail')
+                date = Date.parse(vars['date'].to_s).to_s.split('-')
+                vars['thumbnail_url'] = '/' + File.join('img', date[0], date[1], vars['thumbnail'])
+            end
+
+            vars
+        end
+
+    end
+
 end
