7ac57af5821d5abb48f0aac0795787af2987a5fb
WouterJ <waldio.webdesign@gmail.com>
2013-02-11 17:43:38 +0100

    Added thumbnail support on home

---
+++ _plugins/thumbnail.rb
@@ -0,0 +1,19 @@
+module Jekyll
+
+    class Post
+
+        alias_method :to_liquid_org, :to_liquid
+        def to_liquid
+            vars = to_liquid_org
+
+            if vars.has_key?('thumbnail')
+                date = Date.parse(vars['date'].to_s).to_s.split('-')
+                vars['thumbnail_url'] = '/' + File.join('img', date[0], date[1], vars['thumbnail'])
+            end
+
+            vars
+        end
+
+    end
+
+end
