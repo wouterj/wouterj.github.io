c328afd5f35e5311fd707269067c420e4780e0ef
WouterJ <waldio.webdesign@gmail.com>
2013-09-01 14:25:41 +0200

    Fixed code blocks

--- _plugins/liquid.rb
+++ _plugins/liquid.rb
@@ -4,7 +4,7 @@ module Jekyll
             content = content.sub('<p>', '<p class="post__intro">')
             content = content.sub('</p>', '</p><div class="cf"></div>')
 
-            content = content.gsub('<pre>', '<pre class="prettyprint  linenums">"')
+            content = content.gsub('<pre>', '<pre class="prettyprint  linenums">')
             content = content.gsub('<img', '<img class="img--stripe"')
 
             content
