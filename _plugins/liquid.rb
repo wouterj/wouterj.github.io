15207e5c563af6515af21b64601664e59cb9a1cd
WouterJ <waldio.webdesign@gmail.com>
2013-09-01 15:00:11 +0200

    Added support for post asides

--- _plugins/liquid.rb
+++ _plugins/liquid.rb
@@ -7,6 +7,8 @@ module Jekyll
             content = content.gsub('<pre>', '<pre class="prettyprint  linenums">')
             content = content.gsub('<img', '<img class="img--stripe"')
 
+            content = content.gsub(/<p class='post-side'>(.*?): (.*?)<\/p>/, '<aside class="post-side" data-type="\\1"><p>\\2</p></aside>')
+
             content
         end
     end
