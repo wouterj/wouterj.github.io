5cb2709ae6699fa354f47ee13241e4c715d73f26
WouterJ <waldio.webdesign@gmail.com>
2014-07-29 19:21:40 +0200

    Publish the website, git!

---
+++ publish.sh
@@ -0,0 +1,14 @@
+git config --global user.email 'bot@wouterj.nl'
+git config --global user.name 'WouterJ.nl bot'
+
+git branch -D master
+git checkout -b master
+
+touch output_prod/.nojekyll
+
+git add -f output_prod
+git commit -m "Build website"
+
+git filter-branch --subdirectory-filter output_prod/ -f
+
+git push "https://${GH_TOKEN}@github.com/WouterJ/new-blog"                -
