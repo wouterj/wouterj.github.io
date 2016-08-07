45d77599df0e48aa1f53ba09da8def74112d5806
WouterJ <waldio.webdesign@gmail.com>
2016-08-07 11:58:18 +0200

    Make wouterj package findable

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='wouterj',
+    packages=find_packages(),
     entry_points="""
     [pygments.lexers]
     inline-php = wouterj.pygments.lexers:InlinePhpLexer
