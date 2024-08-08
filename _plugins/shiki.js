8a1ed6471cf08c0e60b016c1de2fc5b39eac8a10
Wouter de Jong <wouter@wouterj.nl>
2024-08-08 13:47:11 +0200

    Add custom highlighting for terminal blocks

--- _plugins/shiki.js
+++ _plugins/shiki.js
@@ -1,5 +1,6 @@
 const shiki = require('shiki');
 const path = require('path');
+const fs = require('fs');
 
 function renderToHtml(lines, options = {}) {
     const skipLines = options.skipLines || 0;
@@ -10,7 +11,7 @@ function renderToHtml(lines, options = {}) {
     let lastLine = Object.keys(lines).length - skipLines;
     lines.forEach((line, lineIndex) => {
         const lineNumber = lineIndex + startLine;
-        if (lineNumber == lastLine || lineIndex < skipLines) {
+        if (lineNumber > lastLine || lineIndex < skipLines) {
             return;
         }
 
@@ -54,20 +55,35 @@ const code = process.argv[2];
 const lang = process.argv[3] || 'none';
 
 if (lang == 'none') {
-    console.log(`<pre class="shiki language-none"><code>${escapeHtml(code)}</code></pre>`);
+    console.log(`<pre class="shiki-gutter">1</pre><pre class="shiki-code"><code>${escapeHtml(code)}</code></pre>`);
 } else {
     (async function (code, lang) {
-        const t = await shiki.loadTheme(path.join(__dirname, 'shiki-theme.json'));
-        const highlighter = await shiki.getHighlighter({ theme: t });
-        let skipLines = 0;
-        if (lang == 'php' && !code.includes('<?')) {
-            code = `<?php\n${code}`;
-            skipLines = 1;
+        try {
+            const t = await shiki.loadTheme(path.join(__dirname, 'shiki-theme.json'));
+            const highlighter = await shiki.getHighlighter({
+                theme: t,
+                langs: [
+                    ...shiki.BUNDLED_LANGUAGES,
+                    {
+                        id: 'terminal',
+                        scopeName: 'source.terminal',
+                        embeddedLangs: ['bash'],
+                        path: path.join(__dirname, 'terminal.json')
+                    }
+                ]
+            });
+            let skipLines = 0;
+            if (lang == 'php' && !code.includes('<?')) {
+                code = `<?php\n${code}`;
+                skipLines = 1;
+            }
+
+            let ast = highlighter.codeToThemedTokens(code.trimEnd(), lang);
+            let html = renderToHtml(ast, { lang, skipLines });
+
+            console.log(html);
+        } catch (e) {
+            console.log(e);
         }
-
-        let ast = highlighter.codeToThemedTokens(code, lang);
-        let html = renderToHtml(ast, { lang, skipLines });
-
-        console.log(html);
     })(code, lang);
 }
